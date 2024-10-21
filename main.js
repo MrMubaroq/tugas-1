c1b2ee906964ddc86211a161ca0cb085278bfb6d
MrMubaroq <mubaroqahmad89@gmail.com>
2024-10-21 13:13:54 +0800

    commit 1

---
+++ main.js
@@ -0,0 +1,7 @@
+let nilaiAwal = prompt("Masukkan Nilai Awal : ");
+let nilaiAkhir = prompt("Masukkan Nilai Akhir : ");
+
+for (let i = nilaiAwal; i <= nilaiAkhir; i++) {
+    console.log(i);
+};
+   
